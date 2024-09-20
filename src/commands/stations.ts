03d003d03a7b0be85aa8ae8afde39f1c118ea74f
Jae Bradley <jae.b.bradley@gmail.com>
2024-09-20 19:23:01 -0400

    Station status data fetching

--- src/commands/stations.ts
+++ src/commands/stations.ts
@@ -2,7 +2,7 @@ import {Command} from "commander";
 import Client from "../thirdparty/bluebikes/client.js";
 import axios from "axios";
 import * as console from "node:console";
-import {StationResponse} from "../thirdparty/bluebikes/types.js";
+import {StationsResponse, StationStatusResponse} from "../thirdparty/bluebikes/types.js";
 
 const stationsCommand = new Command("stations");
 
@@ -11,7 +11,7 @@ stationsCommand
     .argument("<identifier>", "Value can be the station's unique ID, or the station name")
     .action(async (identifier) => {
         const client = new Client(axios.create());
-        let stationResponse: StationResponse;
+        let stationResponse: StationsResponse;
         try {
             stationResponse = await client.getStations();
         } catch (error) {
@@ -29,6 +29,31 @@ stationsCommand
         } else {
             console.log("Could not find matching station for", identifier);
         }
-    })
+    });
+
+stationsCommand
+    .command("status")
+    .argument("<identifier>", "Value can be the station's unique ID, or the station name")
+    .action(async (identifier) => {
+        const client = new Client(axios.create());
+        let stationStatusResponse: StationStatusResponse;
+        try {
+            stationStatusResponse = await client.getStationStatuses();
+        } catch (error) {
+            console.error("An error occurred", error);
+            return;
+        }
+
+        const matchingStation = stationStatusResponse
+            .data
+            .stations
+            .find(station => station.station_id === identifier);
+
+        if (matchingStation) {
+            console.log(matchingStation);
+        } else {
+            console.log("Could not find matching station for", identifier);
+        }
+    });
 
 export default stationsCommand;
